f83b3833742a5fb7ffffa2aad22252fbd59e78ee
Alex Brown <alexanderbrown.info@gmail.com>
2018-06-22 14:51:44 +0100

    handle getMovies action

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,3 +1,13 @@
-const movies = () => ([])
+import { GET_MOVIES } from '../actions'
+import mockMovies from '../../../mocks/movies.json'
+
+const movies = (state = [], action) => {
+  switch (action && action.type) {
+    case GET_MOVIES:
+      return mockMovies
+    default:
+      return state
+  }
+}
 
 export default movies
--- src/redux/reducers/index.spec.js
+++ src/redux/reducers/index.spec.js
@@ -1,9 +1,16 @@
 import movies from './index'
+import { getMovies } from '../actions';
+import mockMovies from '../../../mocks/movies.json'
 
 describe('movies', () => {
   describe('given no arguments', () => {
     it('returns the default state', () => {
       expect(movies()).toEqual([])
+    })
+  })
+  describe('given a getMovies action', () => {
+    it('returns a list of movies', () => {
+      expect(movies([], getMovies('', ''))).toEqual(mockMovies)
     })
   })
 })
