7aac95dd567875b3c8e1c5fbd4e6b9f625e2c083
Alex Brown <alexanderbrown.info@gmail.com>
2018-06-22 14:04:07 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+
+module.exports = {
+  entry: __dirname + '/src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use: [
+          {loader: 'babel-loader'}
+        ]
+      },
+      {
+        test: /\.css$/,
+        exclude: '/node_modules/',
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'}
+        ]
+      }
+    ]
+  }
+}
