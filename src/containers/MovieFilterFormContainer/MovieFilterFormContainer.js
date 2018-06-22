3bde6a9fd0db65c84b072ae1d5c0e4c250a0ab8f
Alex Brown <alexanderbrown.info@gmail.com>
2018-06-22 17:36:30 +0100

    connect MovieFilterForm to redux

---
+++ src/containers/MovieFilterFormContainer/MovieFilterFormContainer.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import MovieFilterForm from '../components/MovieFilterForm/MovieFilterForm'
+import { getMovies } from '../../redux/actions'
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({getMovies}, dispatch)
+
+export default connect(undefined, mapDispatchToProps)(MovieFilterForm)
