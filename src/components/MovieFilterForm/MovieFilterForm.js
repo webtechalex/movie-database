9d4c5cbda77b0a025a0590b81b9930d9cacc3754
Alex Brown <alexanderbrown.info@gmail.com>
2018-06-22 17:29:43 +0100

    implement MovieFilterForm functionality

--- src/components/MovieFilterForm/MovieFilterForm.js
+++ src/components/MovieFilterForm/MovieFilterForm.js
@@ -5,22 +5,76 @@ import TextInput from '../../common/TextInput/TextInput'
 import Button from '../../common/Button/Button'
 
 class MovieFilterForm extends Component {
-  render() {
+  constructor(props) {
+    super(props)
+    this.genreOptions = [
+      {
+        display: 'All',
+        value: 'all'
+      },
+      {
+        display: 'Action',
+        value: 'action'
+      },
+      {
+        display: 'Comedy',
+        value: 'comedy'
+      },
+      {
+        display: 'Drama',
+        value: 'drama'
+      },
+      {
+        display: 'Kids',
+        value: 'kids'
+      },
+      {
+        display: 'Sci-Fi',
+        value: 'sci-fi'
+      }
+    ]
+    this.state = {
+      selectedGenre: '',
+      selectedYear: ''
+    }
+  }
+
+  static propTypes = {
+    getMovies: PropTypes.func.isRequired
+  }
+
+  handleChangeSelectedGenre = (event) => {
+    this.setState({
+      selectedGenre: event.target.value
+    })
+  }
+
+  handleChangeSelectedYear = (event) => {
+    this.setState({
+      selectedYear: event.target.value
+    })
+  }
+
+  handleGetMovies = () => {
+    this.props.getMovies(this.state.selectedGenre, this.state.selectedYear)
+  }
+
+  render = () => {
     return (
       <Fragment>
         <SelectDropdown
           value=''
-          handleChange={() => {}}
-          options={[]}
+          handleChange={this.handleChangeSelectedGenre}
+          options={this.genreOptions}
         />
-      <TextInput
-        value=''
-        handleChange={() => {}}
-      />
-      <Button
-        text='Get Movies'
-        handleClick={() => {}}
-      />
+        <TextInput
+          value=''
+          handleChange={this.handleChangeSelectedYear}
+        />
+        <Button
+          text='Get Movies'
+          handleClick={this.handleGetMovies}
+        />
       </Fragment>
     )
   }
--- src/components/MovieFilterForm/MovieFilterForm.spec.js
+++ src/components/MovieFilterForm/MovieFilterForm.spec.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import MovieFilterForm from './MovieFilterForm'
 import SelectDropdown from '../../common/SelectDropdown/SelectDropdown'
 import TextInput from '../../common/TextInput/TextInput'
 import Button from '../../common/Button/Button'
 
 describe('MovieFilterForm', () => {
-  const wrapper = shallow(<MovieFilterForm/>)
+  const wrapper = shallow(
+    <MovieFilterForm
+      getMovies={jest.fn()}
+    />
+  )
+  const instance = wrapper.instance()
   it('renders a SelectDropdown', () => {
     expect(wrapper.find(SelectDropdown).length).toBe(1)
   })
@@ -15,5 +20,48 @@ describe('MovieFilterForm', () => {
   })
   it('renders a Button', () => {
     expect(wrapper.find(Button).length).toBe(1)
+  })
+  it('has a class property: genreOptions', () => {
+    const genreOptions = [
+      {
+        display: 'All',
+        value: 'all'
+      },
+      {
+        display: 'Action',
+        value: 'action'
+      },
+      {
+        display: 'Comedy',
+        value: 'comedy'
+      },
+      {
+        display: 'Drama',
+        value: 'drama'
+      },
+      {
+        display: 'Kids',
+        value: 'kids'
+      },
+      {
+        display: 'Sci-Fi',
+        value: 'sci-fi'
+      }
+    ]
+    expect(instance).toHaveProperty('genreOptions', genreOptions)
+  })
+  it('has a state property for selectedGenre', () => {
+    expect(wrapper.state()).toHaveProperty('selectedGenre', '')
+  })
+  it('updates selectedGenre when handleChangeSelectedGenre is called', () => {
+    instance.handleChangeSelectedGenre({ target: { value: 'action' } })
+    expect(wrapper.state('selectedGenre')).toBe('action')
+  })
+  it('has a state property for selectedYear', () => {
+    expect(wrapper.state()).toHaveProperty('selectedYear', '') 
+  })
+  it('updates selectedYear when handleChangeSelectedYear is called', () => {
+    instance.handleChangeSelectedYear({ target: { value: '1999' } })
+    expect(wrapper.state('selectedYear')).toBe('1999')
   })
 })
